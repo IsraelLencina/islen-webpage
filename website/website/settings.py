69ac8f26784a70d1b9e09d6a3296a0f183fe1952
Israel Lencina Navarro <israel.lencina@ikostech.es>
2024-09-23 11:30:45 +0200

    Added mainpage, added support for static files.

--- website/website/settings.py
+++ website/website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "mainpage"
 ]
 
 MIDDLEWARE = [
@@ -117,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
