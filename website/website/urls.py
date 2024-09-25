8b93bb59cc6a23bc134bb58f2e8b1e56a05eafb4
Israel Lencina Navarro <israel.lencina@ikostech.es>
2024-09-25 08:57:34 +0200

    Multi traduction begin, config and first traductions.

--- website/website/urls.py
+++ website/website/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 import sys
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mainpage.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),  # Para manejar el cambio de idioma
 ]
+
+urlpatterns += [
+    path('admin/', admin.site.urls)
+]
+
+urlpatterns += i18n_patterns(
+    path('', include('mainpage.urls')),  # Ajusta las URLs de tu aplicación
+)
